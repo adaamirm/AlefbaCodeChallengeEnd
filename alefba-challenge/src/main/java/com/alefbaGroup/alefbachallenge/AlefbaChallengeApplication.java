45d7058a76a00e49ac3f8b1b3f8e886f9ee2bf58
Mahdi Moaddel <74235792+adaamirm@users.noreply.github.com>
2023-06-02 03:31:22 -0700

    Add files via upload

---
+++ alefba-challenge/src/main/java/com/alefbaGroup/alefbachallenge/AlefbaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alefbaGroup.alefbachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlefbaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlefbaChallengeApplication.class, args);
+	}
+
+}
